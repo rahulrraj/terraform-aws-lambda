56256d3cebe566028c29ef1245bcddf752a1a5d0
rahulrraj <rahul_rraj@yahoo.com>
2020-03-15 21:33:16 -0400

    Update main.tf

--- examples/simple-routing/main.tf
+++ examples/simple-routing/main.tf
@@ -8,9 +8,7 @@ module "api_with_simple_routing" {
   version                     = "0.0.2"
   name                        = "${var.name}-simple"
   vpc_id                      = "${var.aws_vpc_id}"
-  secondary_vpc_id            = "${var.aws_secondary_vpc_id}"
   subnets                     = ["${var.aws_vpc_subnet_primary}", "${var.aws_vpc_subnet_secondary}"]
-  secondary_subnets           = ["${var.aws_vpc_subnet_dr_primary}", "${var.aws_vpc_subnet_dr_secondary}"]
   fqdn                        = "${var.name}-simple.${var.acadian_domain}"
   zone_id                     = "${var.aws_zone_id}"
   lambda_zip_file_fullname    = "${local.lambda_zip_file_fullname}"
